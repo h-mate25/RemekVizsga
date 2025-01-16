2db131ac40f797d5e389936d29cef7bee080b48c
Horvát Máté <horvat.mate2005@gmail.com>
2025-01-16 10:48:57 +0100

    Add files via upload

---
+++ RakKezelo_v0.0.0/RakKezelo_v0.0.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RakKezelo_v0._0._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
