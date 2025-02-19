870416b6a90dcb26601b8c9358a0c38dc158c1e7
User <User@2410W10>
2025-02-19 13:04:43 +0100

    Base of the DatabseAdd project files.

---
+++ Model/Process.cs
@@ -0,0 +1,21 @@
+﻿namespace Model
+{
+    public class Process
+    {
+        public int id { get; set; }
+
+        public string type { get; set; }
+
+        public DateTime date { get; set; }
+
+        public List<int> pallet_id { get; set; }
+
+        public List<int> product_id { get; set; }
+
+        public List<int> worker_id { get; set; }
+
+        public List<int> incomingCargo_id { get; set; }
+
+        public List<int> outgoingCargo_id { get; set; }
+    }
+}
