8db7aa02d328fa7416dcb417734aad1617eaf6f4
Bottyán Vak <ecdl01@vbjnet.hu>
2025-02-26 09:16:47 +0100

    na most

---
+++ Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string quantity { get; set; }
+
+        public  string location { get; set; }
+    }
+}
