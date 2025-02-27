f90665933904892746eef0e1d07112a2b2265abc
Bottyán Vak <ecdl01@vbjnet.hu>
2025-02-27 12:31:09 +0100

    sirok is

---
+++ Model/Implemantations/ProcessDatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Implemantations
+{
+    public class ProcessDatabaseManager : ProcessManagerInterface
+    {
+        private WarehouseDbContext dbContext;
+
+        public ProcessDatabaseManager(WarehouseDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void CreateProcess(Process process)
+        {
+            process.id = 0;
+            dbContext.Processes.Add(process);
+            dbContext.SaveChanges();
+        }
+
+        public List<Process> ReadProcess()
+        {
+            return dbContext.Processes.ToList();
+        }
+
+
+
+    }
+}
---
+++ Model/ProcessManagerInterface.cs
@@ -0,0 +1,15 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public interface ProcessManagerInterface
+    {
+        List<Process> ReadProcess();
+        void CreateProcess(Process process);
+    }
+}
